d5b121cdebb037153d755d5f8fbe4eff6a6d7b1d
susooo <92291198+susooo@users.noreply.github.com>
2024-10-02 08:11:12 +0900

    Feat: matePost 생성, 수정

--- src/main/java/com/boot/gugi/repository/MatePostRepository.java
+++ src/main/java/com/boot/gugi/repository/MatePostRepository.java
@@ -1,18 +1,8 @@
 package com.boot.gugi.repository;
 
-import com.boot.gugi.base.Enum.AgeGroup;
-import com.boot.gugi.base.Enum.GenderPreference;
-import com.boot.gugi.base.Enum.Stadium;
 import com.boot.gugi.model.MatePost;
-import com.boot.gugi.model.User;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
 
 @Repository
 public interface MatePostRepository extends JpaRepository<MatePost, Long>, MatePostRepositoryCustom {
