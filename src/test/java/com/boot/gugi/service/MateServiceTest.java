5295861959c38e4af7a88c5f01b04432e233dba9
susooo <92291198+susooo@users.noreply.github.com>
2024-10-04 01:30:36 +0900

    Test: mate post 생성, 수정 입력값 변경(한글 입력을 영어 변환 후 DB저장)

--- src/test/java/com/boot/gugi/service/MateServiceTest.java
+++ src/test/java/com/boot/gugi/service/MateServiceTest.java
@@ -7,6 +7,7 @@ import com.boot.gugi.base.Enum.*;
 import com.boot.gugi.base.dto.MateDTO;
 import com.boot.gugi.base.dto.MateRequestDTO;
 import com.boot.gugi.base.dto.MateResponseDTO;
+import com.boot.gugi.base.util.TranslationUtil;
 import com.boot.gugi.exception.MatePostException;
 import com.boot.gugi.model.MatePost;
 import com.boot.gugi.model.MatePostApplicant;
@@ -52,12 +53,16 @@ public class MateServiceTest {
 
     @BeforeEach
     public void setUp() {
-        // Given User
-        owner = createUser("김구기", "test@example.com", Sex.FEMALE, 25, Team.SAMSUNG);
-        applicant = createUser("이철수", "applicant@example.com", Sex.MALE, 29, Team.SAMSUNG);
+        // Given
+        owner = createUser("이꾸", "test@example.com", "여자", 25, "삼성");
+        applicant = createUser("김구기", "applicant@example.com", "남자", 29, "한화");
+
+        createdPost = createMatePost("Match Title", "Match Content", "contact@example.com", 4,
+                LocalDate.now().plusDays(1), "대구 삼성 라이온즈 파크",
+                "여자만", "20대", "삼성", owner);
     }
 
-    private User createUser(String name, String email, Sex gender, int age, Team team) {
+    private User createUser(String name, String email, String gender, Integer age, String team) {
         User user = new User();
         user.setName(name);
         user.setEmail(email);
@@ -68,8 +73,8 @@ public class MateServiceTest {
     }
 
     private MateDTO createMateDTO(String title, String content, String contact,
-                                  int totalMembers, LocalDate gameDate, Stadium stadium,
-                                  GenderPreference gender, AgeGroup ageGroup, Team team) {
+                                  Integer totalMembers, LocalDate gameDate, String stadium,
+                                  String gender, String ageGroup, String team) {
         MateDTO dto = new MateDTO();
         dto.setTitle(title);
         dto.setContent(content);
@@ -84,8 +89,8 @@ public class MateServiceTest {
     }
 
     private MatePost createMatePost(String title, String content, String contact,
-                                    int totalMembers, LocalDate gameDate, Stadium stadium,
-                                    GenderPreference gender, AgeGroup ageGroup, Team team, User owner) {
+                                    Integer totalMembers, LocalDate gameDate, String stadium,
+                                    String gender, String ageGroup, String team, User owner) {
         MateDTO dto = createMateDTO(title, content, contact, totalMembers, gameDate, stadium, gender, ageGroup, team);
         return matePostService.createMatePost(dto, owner);
     }
@@ -93,11 +98,6 @@ public class MateServiceTest {
     @Test
     @DisplayName("직관메이트 글 생성")
     public void createMatePostTest() {
-        // Given
-        createdPost = createMatePost("Match Title", "Match Content", "contact@example.com", 4,
-                LocalDate.now().plusDays(1), Stadium.DAEGU,
-                GenderPreference.FEMALE_ONLY, AgeGroup.AGE_20s, Team.SAMSUNG, owner);
-
         // Then
         assertThat(createdPost).isNotNull();
         assertThat(createdPost.getParticipants()).isEqualTo(1);
@@ -105,6 +105,10 @@ public class MateServiceTest {
         assertThat(createdPost.getTitle()).isEqualTo("Match Title");
         assertThat(createdPost.getContent()).isEqualTo("Match Content");
         assertThat(createdPost.getContact()).isEqualTo("contact@example.com");
+        assertThat(createdPost.getStadium()).isEqualTo("DAEGU");
+        assertThat(createdPost.getGender()).isEqualTo("FEMALE_ONLY");
+        assertThat(createdPost.getAge()).isEqualTo("AGE_20s");
+        assertThat(createdPost.getTeam()).isEqualTo("SAMSUNG");
 
         // Verify
         MatePostStatus postStatus = matePostStatusRepository.findByUser(owner).stream().findFirst().orElse(null);
@@ -114,22 +118,19 @@ public class MateServiceTest {
         assertThat(postStatus.getUser().getId()).isEqualTo(owner.getId());
     }
 
+
     @Test
     @DisplayName("직관메이트 글 수정")
     public void updateMatePostTest() {
         // Given
-        createdPost = createMatePost("Match Title", "Match Content", "contact@example.com", 4,
-                LocalDate.now().plusDays(1), Stadium.DAEGU,
-                GenderPreference.FEMALE_ONLY, AgeGroup.AGE_20s, Team.SAMSUNG, owner);
-
         UUID postId = createdPost.getId();
         MateDTO updateDto = new MateDTO();
         updateDto.setTitle("Updated Title");
         updateDto.setContent("Updated Content");
         updateDto.setGameDate(LocalDate.now().plusDays(2));
-        updateDto.setStadium(Stadium.SUWON);
+        updateDto.setStadium("광주 기아 챔피언스 필드");
         updateDto.setTotalMembers(5);
-        updateDto.setTeam(Team.KIA);
+        updateDto.setTeam("KIA");
 
         // When
         MatePost updatedPost = matePostService.updateMatePost(postId, updateDto);
@@ -142,18 +143,14 @@ public class MateServiceTest {
         assertThat(updatedPost.getContent()).isEqualTo("Updated Content");
         assertThat(updatedPost.getContact()).isEqualTo(createdPost.getContact());
         assertThat(updatedPost.getGameDate()).isEqualTo(LocalDate.now().plusDays(2));
-        assertThat(updatedPost.getStadium()).isEqualTo(Stadium.SUWON);
-        assertThat(updatedPost.getTeam()).isEqualTo(Team.KIA);
+        assertThat(updatedPost.getStadium()).isEqualTo("GWANGJU");
+        assertThat(updatedPost.getTeam()).isEqualTo("KIA");
     }
 
     @Test
     @DisplayName("직관메이트 매칭 신청")
     public void applyForMatePostTest() {
         // Given
-        createdPost = createMatePost("Match Title", "Match Content", "contact@example.com", 4,
-                LocalDate.now().plusDays(1), Stadium.DAEGU,
-                GenderPreference.FEMALE_ONLY, AgeGroup.AGE_20s, Team.SAMSUNG, owner);
-
         MateRequestDTO requestDTO = new MateRequestDTO();
         requestDTO.setPostId(createdPost.getId());
         requestDTO.setUserId(applicant.getId());
@@ -180,10 +177,6 @@ public class MateServiceTest {
     @DisplayName("직관메이트 매칭 신청 - 본인 글 신청 에러")
     public void applyForMatePost_OwnerCannotApplyTest() {
         // Given
-        createdPost = createMatePost("Match Title", "Match Content", "contact@example.com", 4,
-                LocalDate.now().plusDays(1), Stadium.DAEGU,
-                GenderPreference.FEMALE_ONLY, AgeGroup.AGE_20s, Team.SAMSUNG, owner);
-
         MateRequestDTO requestDTO = new MateRequestDTO();
         requestDTO.setPostId(createdPost.getId());
         requestDTO.setUserId(owner.getId());
@@ -198,10 +191,6 @@ public class MateServiceTest {
     @DisplayName("직관메이트 매칭 승인")
     public void approveApplicationTest() {
         // Given
-        createdPost = createMatePost("Match Title", "Match Content", "contact@example.com", 4,
-                LocalDate.now().plusDays(1), Stadium.DAEGU,
-                GenderPreference.FEMALE_ONLY, AgeGroup.AGE_20s, Team.SAMSUNG, owner);
-
         MateRequestDTO requestDTO = new MateRequestDTO();
         requestDTO.setPostId(createdPost.getId());
         requestDTO.setUserId(applicant.getId());
@@ -230,10 +219,6 @@ public class MateServiceTest {
     @DisplayName("직관메이트 신청 승인 - 모집 완료 에러")
     public void approveApplication_PostFullTest() {
         // Given
-        createdPost = createMatePost("Match Title", "Match Content", "contact@example.com", 4,
-                LocalDate.now().plusDays(1), Stadium.DAEGU,
-                GenderPreference.FEMALE_ONLY, AgeGroup.AGE_20s, Team.SAMSUNG, owner);
-
         createdPost.setParticipants(4);
         MateRequestDTO requestDTO = new MateRequestDTO();
         requestDTO.setPostId(createdPost.getId());
@@ -254,10 +239,6 @@ public class MateServiceTest {
     @DisplayName("직관메이트 신청 거절")
     public void rejectApplicationTest() {
         // Given
-        createdPost = createMatePost("Match Title", "Match Content", "contact@example.com", 4,
-                LocalDate.now().plusDays(1), Stadium.DAEGU,
-                GenderPreference.FEMALE_ONLY, AgeGroup.AGE_20s, Team.SAMSUNG, owner);
-
         MateRequestDTO requestDTO = new MateRequestDTO();
         requestDTO.setPostId(createdPost.getId());
         requestDTO.setUserId(applicant.getId());
